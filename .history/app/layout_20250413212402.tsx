0fc42cd9918e80562f70742f297b3cc2e86c960e
Lamat Abdellahi <lamat032025@gmail.com>
2025-04-13 21:24:19 +0100

    add

--- .history/app/layout_20250413212402.tsx
+++ .history/app/layout_20250413212402.tsx
@@ -11,7 +11,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Union des Sortants  d'Algérie",
   description: "Application",
-  icons: "/union.jpg",
+  icons: "union.jpg",
 };
 
 export default function RootLayout({
